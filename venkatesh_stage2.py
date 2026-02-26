9c3b78c8a299c86c24a52beaa6cba00829ca251a
venkat1225-tom <108568731+venkat1225-tom@users.noreply.github.com>
2026-02-26 12:02:11 +0530

    Implement basic calculator functionality

---
+++ venkatesh_stage2.py
@@ -0,0 +1,34 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    result = num1 + num2
+
+elif operator == "-":
+    result = num1 - num2
+
+elif operator == "*":
+    result = num1 * num2
+
+elif operator == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed")
+        exit()
+    else:
+        result = num1 / num2
+
+else:
+    print("Invalid operator")
+    exit()
+
+print("Result =", result)
+
+# Result check
+if result > 0:
+    print("Positive")
+elif result < 0:
+    print("Negative")
+else:
+    print("Zero")
+
